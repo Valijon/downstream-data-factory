19e64d475a51e5542def07791c925a3ddece4b48
Valijon Nazarov <v.nazarov@indizen.com>
2023-08-01 17:21:13 +0200

    s

--- src/main/java/com/pru/globalpayments/feeds/downstream/anothersystem/AnotherSystemDataFactoryApplication.java
+++ src/main/java/com/pru/globalpayments/feeds/downstream/anothersystem/AnotherSystemDataFactoryApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AnotherSystemDataFactoryApplication {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		System.out.println("AnotherSystemDataFactoryApplication running...");
 		SpringApplication.run(AnotherSystemDataFactoryApplication.class, args);
 	}
